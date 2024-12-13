96f27472d60b3dc8ad1e7625f11d44855f5d04ff
Moritz Lang <16192401+slashmo@users.noreply.github.com>
2024-12-13 22:29:54 +0100

    Generate OFREP OpenAPI client

--- Package.swift
+++ Package.swift
@@ -13,13 +13,16 @@ let package = Package(
         .library(name: "OFREP", targets: ["OFREP"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-open-feature/swift-open-feature.git", branch: "main")
+        .package(url: "https://github.com/swift-open-feature/swift-open-feature.git", branch: "main"),
+        .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "OFREP",
             dependencies: [
-                .product(name: "OpenFeature", package: "swift-open-feature")
+                .product(name: "OpenFeature", package: "swift-open-feature"),
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
             ]
         ),
         .testTarget(
