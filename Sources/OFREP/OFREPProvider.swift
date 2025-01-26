30d301bae93502b2185f3963f96b4500cf6d19e6
Moritz Lang <16192401+slashmo@users.noreply.github.com>
2025-01-26 20:40:38 +0100

    Add graceful shutdown to OFREP provider

--- Sources/OFREP/OFREPProvider.swift
+++ Sources/OFREP/OFREPProvider.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift OpenFeature open source project
 //
-// Copyright (c) 2024 the Swift OpenFeature project authors
+// Copyright (c) 2025 the Swift OpenFeature project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -10,3 +10,33 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
+
+import Logging
+import OpenFeature
+import ServiceLifecycle
+
+public struct OFREPProvider<Transport: OFREPClientTransport>: OpenFeatureProvider, CustomStringConvertible {
+    public let metadata = OpenFeatureProviderMetadata(name: "OpenFeature Remote Evaluation Protocol Provider")
+    public let description = "OFREPProvider"
+    private let transport: Transport
+    private let logger = Logger(label: "OFREPProvider")
+
+    package init(transport: Transport) {
+        self.transport = transport
+    }
+
+    public func resolution(
+        of flag: String,
+        defaultValue: Bool,
+        context: OpenFeatureEvaluationContext?
+    ) async -> OpenFeatureResolution<Bool> {
+        OpenFeatureResolution(value: defaultValue)
+    }
+
+    public func run() async throws {
+        try await gracefulShutdown()
+        logger.debug("Shutting down.")
+        try await transport.shutdownGracefully()
+        logger.debug("Shut down.")
+    }
+}
--- Tests/OFREPTests/OFREPProviderTests.swift
+++ Tests/OFREPTests/OFREPProviderTests.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the Swift OpenFeature open source project
 //
-// Copyright (c) 2024 the Swift OpenFeature project authors
+// Copyright (c) 2025 the Swift OpenFeature project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -10,3 +10,75 @@
 // SPDX-License-Identifier: Apache-2.0
 //
 //===----------------------------------------------------------------------===//
+
+import Foundation
+import HTTPTypes
+import OFREP
+import OpenAPIRuntime
+import Testing
+import ServiceLifecycle
+@testable import Logging
+
+@Suite("OFREP Provider")
+final class OFREPProviderTests {
+    init() {
+        LoggingSystem.bootstrapInternal { label in
+            var handler = StreamLogHandler.standardOutput(label: label)
+            handler.logLevel = .debug
+            return handler
+        }
+    }
+
+    deinit {
+        LoggingSystem.bootstrapInternal(SwiftLogNoOpLogHandler.init)
+    }
+
+    @Test("Graceful shutdown")
+    func shutsDownTransport() async throws {
+        /// A no-op service which is used to shut down the service group upon successful termination.
+        struct ShutdownTrigger: Service, CustomStringConvertible {
+            let description = "ShutdownTrigger"
+
+            func run() async throws {}
+        }
+
+        let transport = RecordingOFREPClientTransport()
+        let provider = OFREPProvider(transport: transport)
+
+        await #expect(transport.numberOfShutdownCalls == 0)
+
+        let group = ServiceGroup(
+            configuration: .init(
+                services: [
+                    .init(service: provider),
+                    .init(service: ShutdownTrigger(), successTerminationBehavior: .gracefullyShutdownGroup),
+                ],
+                logger: Logger(label: "test")
+            )
+        )
+
+        try await group.run()
+
+        await #expect(transport.numberOfShutdownCalls == 1)
+    }
+}
+
+private actor RecordingOFREPClientTransport: OFREPClientTransport {
+    var numberOfShutdownCalls = 0
+
+    func send(
+        _ request: HTTPRequest,
+        body: HTTPBody?,
+        baseURL: URL,
+        operationID: String
+    ) async throws -> (
+        HTTPResponse,
+        HTTPBody?
+    ) {
+        (HTTPResponse(status: 418), nil)
+    }
+
+    func shutdownGracefully() async throws {
+        numberOfShutdownCalls += 1
+    }
+}
